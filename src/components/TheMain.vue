096ef63e12770a7afeb756c71add8fd00755d94a
SirGoPadawan <orangehardcore0@gmail.com>
2020-08-22 06:21:29 +0300

    новый компонент

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,12 +1,14 @@
 <template>
   <main class="main">
     <v-brands />
+    <v-news />
   </main>
 </template>
 <script>
 export default {
   components: {
     "v-brands": () => import("./VBrands"),
+    "v-news": () => import("./VNews"),
   },
 };
 </script>
---
+++ src/components/VNews.vue
@@ -0,0 +1,80 @@
+<template>
+  <section class="news">
+    <h2 class="news__title">Новости производителей</h2>
+    <div class="news__list">
+      <div class="news__item">
+        <div class="news__img-box">
+          <img src="../img/news1.png" alt="первая новость" class="news__img" />
+        </div>
+        <div class="news__brand-date">
+          <p class="news__brand">Abat</p>
+          <p class="news__date">22.06.17</p>
+        </div>
+        <h3
+          class="news__item-title"
+        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, voluptatibus!</h3>
+        <p class="news__text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam autem tenetur commodi nisi maiores ipsam magni
+          fuga alias excepturi harum eius error ipsum et debitis tempora, provident blanditiis temporibus vitae quia modi
+          atque! Consequuntur nesciunt id sint quasi tempora, earum illum officiis ipsum, minus at alias repellendus
+          facere iusto dolor!
+        </p>
+      </div>
+      <div class="news__item">
+        <div class="news__img-box">
+          <img src="../img/news2.png" alt="первая новость" class="news__img" />
+        </div>
+        <div class="news__brand-date">
+          <p class="news__brand">Abat</p>
+          <p class="news__date">22.06.17</p>
+        </div>
+        <h3
+          class="news__item-title"
+        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, voluptatibus!</h3>
+        <p class="news__text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam autem tenetur commodi nisi maiores ipsam magni
+          fuga alias excepturi harum eius error ipsum et debitis tempora, provident blanditiis temporibus vitae quia modi
+          atque! Consequuntur nesciunt id sint quasi tempora, earum illum officiis ipsum, minus at alias repellendus
+          facere iusto dolor!
+        </p>
+      </div>
+      <div class="news__item">
+        <div class="news__img-box">
+          <img src="../img/news3.png" alt="первая новость" class="news__img" />
+        </div>
+        <div class="news__brand-date">
+          <p class="news__brand">Abat</p>
+          <p class="news__date">22.06.17</p>
+        </div>
+        <h3
+          class="news__item-title"
+        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, voluptatibus!</h3>
+        <p class="news__text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam autem tenetur commodi nisi maiores ipsam magni
+          fuga alias excepturi harum eius error ipsum et debitis tempora, provident blanditiis temporibus vitae quia modi
+          atque! Consequuntur nesciunt id sint quasi tempora, earum illum officiis ipsum, minus at alias repellendus
+          facere iusto dolor!
+        </p>
+      </div>
+      <div class="news__item">
+        <div class="news__img-box">
+          <img src="../img/news4.png" alt="первая новость" class="news__img" />
+        </div>
+        <div class="news__brand-date">
+          <p class="news__brand">Abat</p>
+          <p class="news__date">22.06.17</p>
+        </div>
+        <h3
+          class="news__item-title"
+        >Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, voluptatibus!</h3>
+        <p class="news__text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam autem tenetur commodi nisi maiores ipsam magni
+          fuga alias excepturi harum eius error ipsum et debitis tempora, provident blanditiis temporibus vitae quia modi
+          atque! Consequuntur nesciunt id sint quasi tempora, earum illum officiis ipsum, minus at alias repellendus
+          facere iusto dolor!
+        </p>
+      </div>
+    </div>
+    <button class="news__button">Все новости</button>
+  </section>
+</template>
